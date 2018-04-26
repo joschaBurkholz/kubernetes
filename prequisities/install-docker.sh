88d70dbda6fb431b004c7a3cc83285f0418a4861
Joscha Burkholz <joscha.burkholz@codecentric.de>
2018-04-26 18:29:23 +0200

    fix script

--- prequisities/install-docker.sh
+++ prequisities/install-docker.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-apt-get update && apt-get install -y \
-    apt-transport-https  \
-    ca-certificates  \
-    curl \ 
+apt-get update && apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
-apt-get update && apt-get install docker-ce
+apt-get update && apt-get install -y --no-install-recommends \
+    docker-ce
