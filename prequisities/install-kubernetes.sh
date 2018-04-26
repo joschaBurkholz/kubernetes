ab176016d6b17f9b18e0d64fd16de9c9d4c2b01f
Joscha Burkholz <joscha.burkholz@codecentric.de>
2018-04-26 20:59:50 +0200

    formatting

--- prequisities/install-kubernetes.sh
+++ prequisities/install-kubernetes.sh
@@ -6,7 +6,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update && apt-get install -y --no-install-recommends \
-kubelet \
-kubeadm \
+apt-get update && apt-get install -y --no-install-recommends\
+kubelet\
+kubeadm\
  kubernetes-cni
