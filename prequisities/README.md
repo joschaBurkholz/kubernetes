e18fca9984ffc3149417dbfd553fe8715f8965ef
Joscha Burkholz <joscha.burkholz@codecentric.de>
2018-04-26 20:44:55 +0200

    formatting

--- prequisities/README.md
+++ prequisities/README.md
@@ -2,7 +2,7 @@
 
 ## Install Docker
 
-`curl -sL https://raw.githubusercontent.com/joschaBurkholz/kubernetes/master/prequisities/install-docker.sh | sudo sh`
+```curl -sL https://raw.githubusercontent.com/joschaBurkholz/kubernetes/master/prequisities/install-docker.sh | sudo sh```
 
 ## Install Kubernetes
 
--- prequisities/install-kubernetes.sh
+++ prequisities/install-kubernetes.sh
@@ -6,4 +6,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update && apt-get install -y --no-install-recommends kubelet kubeadm  kubernetes-cni
+apt-get update && apt-get install -y --no-install-recommends \
+    kubelet \
+    kubeadm \
+     kubernetes-cni
