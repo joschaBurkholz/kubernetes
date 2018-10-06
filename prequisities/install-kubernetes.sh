3d2f600ded03402d4eb747b43b2b67c31ef3afc5
Joscha Burkholz <joscha.burkholz@codecentric.de>
2018-10-06 16:03:35 +0200

    Update install-kubernetes.sh

--- prequisities/install-kubernetes.sh
+++ prequisities/install-kubernetes.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get install -y --no-install-recommends apt-transport-https
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-echo "deb http://apt.kubernetes.io/ kubernetes-bionic main"  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update && apt-get install -y --no-install-recommends \
     kubelet \
