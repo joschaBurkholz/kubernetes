1c435efc6f9dde4f61f6a9bc23f3a50153f83238
Joscha Burkholz <joscha.burkholz@codecentric.de>
2018-10-10 13:38:52 +0200

    Pin kubernetes-cni to 0.6.0-00
    Latest cni version currently 0.6.0-02 but kubelet (1.12.1) needs 0.6.0-00

--- prequisities/install-kubernetes.sh
+++ prequisities/install-kubernetes.sh
@@ -5,8 +5,8 @@ apt-get update && apt-get install -y --no-install-recommends apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 apt-get update && apt-get install -y --no-install-recommends \
     kubelet \
     kubeadm \
-    kubernetes-cni
+    kubernetes-cni=0.6.0-00
